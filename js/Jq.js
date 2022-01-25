e101508f301e7658f618dfbb79902b64b32d55c3
JabeerGulam <jabeergulam2@gmail.com>
2022-01-25 22:11:06 +0800

    Cat Gallery Done

--- js/Jq.js
+++ js/Jq.js
@@ -39,21 +39,36 @@ $(document).ready(function(){
             $("ul").toggleClass("showNav");
           }); 
           $(".btnLoadMore").click(function(){
-
-        
-
-                fetch("https://api.thecatapi.com/v1/images/search?breed_ids=rblu&limit=4")
-                .then(res => res.json())
-                .then( (data)  => {
-                    var text = ''
-                    data.forEach(element => {
-                   text += `<div><img src="${element.url}" alt="CAT"> </div>`
-                         console.log(element.url)
-                          $(".randomCat").append(`<img src="${element.url}" style="height:300px;">`) 
+              alert("Loading...")
+                // fetch("https://api.thecatapi.com/v1/images/search?breed_ids=rblu&limit=4")
+                // .then(res => res.json())
+                // .then( (data)  => {
+                //     var text = ''
+                //     data.forEach(element => {
+                //    text += `<div><img src="${element.url}" alt="CAT"> </div>`
+                //          console.log(element.url)
+                //           $(".randomCat").append(`<img src="${element.url}" style="height:300px;">`) 
                     
-                    })
-                        })  
+                //     })
+                //         })  
         //     )
+        $('.gallery').show()
+        fetch("https://api.thecatapi.com/v1/images/search?breed_ids=rblu&limit=7")
+        .then(res => res.json())
+        .then( item => {
+                   
+                   var text = ""
+                   item.forEach(element => {
+                   text += `  <div><img src="${element.url}" alt=""></div>`
+                   $(' #wrapper').html(text)
+     
+                
+                  
+                   
+                 });
+             })
+             .catch( err => console.log(err))
+     
           }); 
       });
 
